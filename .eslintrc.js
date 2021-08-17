9b486b21891e3da7b2b9d6bc1452253163dc1aaa
Nina Motorna <nina.motorna@gmail.com>
2021-08-18 00:56:07 +0200

    Inits the project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+export default {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ contacts.js
@@ -0,0 +1,5 @@
+const fs = require("fs").promises;
+const url = require("url");
+const path = require("path");
+
+const contactsPath = fs.readFile("");
